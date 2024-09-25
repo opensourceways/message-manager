04fce82cb0aa9369ddd45e2cf4c5905a3805034e
毛毛 <5427081+maomao19970922@user.noreply.gitee.com>
2024-09-25 16:57:58 +0800

    会议时间筛选

--- utils/util.go
+++ utils/util.go
@@ -40,7 +40,8 @@ func ParseUnixTimestamp(timestampStr string) *time.Time {
 	seconds := timestamp / 1000
 	nanoseconds := (timestamp % 1000) * 1000000
 	t := time.Unix(seconds, nanoseconds)
-	return &t
+	utcPlus8 := t.Add(8 * time.Hour)
+	return &utcPlus8
 }
 
 func IsEurMessage(source string) bool {
