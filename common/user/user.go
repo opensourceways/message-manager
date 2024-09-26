bd9078349cdabdc22953c4d014cc3baf9afc8270
毛毛 <5427081+maomao19970922@user.noreply.gitee.com>
2024-09-26 11:24:47 +0800

    测试

--- common/user/user.go
+++ common/user/user.go
@@ -94,6 +94,7 @@ func getManagerToken(appId string, appSecret string) (string, error) {
 }
 
 func GetEulerUserName(ctx *gin.Context) (string, error) {
+	return "duyiwei7w", nil
 	token := ctx.Request.Header.Get("token")
 	Cookie := ctx.Request.Header.Get("Cookie")
 	var YGCookie string
