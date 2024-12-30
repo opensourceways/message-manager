e7b40eb05b98b4628ab4a84385b5c53df9382d32
Hourunze1997 <1043170898@qq.com>
2024-12-30 19:41:37 +0800

    sqlfix

--- common/cassandra/config.go
+++ common/cassandra/config.go
@@ -5,9 +5,9 @@ Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved
 package cassandra
 
 type Config struct {
-	Host string `json:"host"     required:"true"`
-	User string `json:"user"     required:"true"`
-	Pwd  string `json:"pwd"      required:"true"`
-	Name string `json:"name"     required:"true"`
-	Port int    `json:"port"     required:"true"`
+	Host     string `json:"host"     required:"true"`
+	User     string `json:"user"     required:"true"`
+	Password string `json:"pwd"      required:"true"`
+	Name     string `json:"name"     required:"true"`
+	Port     int    `json:"port"     required:"true"`
 }
